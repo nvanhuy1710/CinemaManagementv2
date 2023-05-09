50209fc14e3042bc7c49e311f8e5a76d68f30cf3
nvanhuy1710 <nvanhuy1710@gmail.com>
2023-05-09 15:50:52 +0700

    fix token exist 60'

--- Cinema/Helper/TokenProvider.cs
+++ Cinema/Helper/TokenProvider.cs
@@ -30,7 +30,7 @@ namespace Cinema.Helper
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
                 _config["Jwt:Audience"],
                 claims,
-                expires: DateTime.Now.AddMinutes(15),
+                expires: DateTime.Now.AddMinutes(60),
                 signingCredentials: credentials);
 
 
